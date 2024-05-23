6e3bc448a3de90e4bfae0cc8b6f720bea474b4a6
addper345 <25126375+addper345@users.noreply.github.com>
2024-05-23 19:57:29 -0400

    created playRound() function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -17,3 +17,49 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice=='rock' && computerChoice=='paper')
+    {
+      computerScore +=1;
+      console.log("Computer wins!");
+    }
+    if(humanChoice=='rock' && computerChoice=='scissors')
+    {
+        humanScore +=1;
+        console.log("Human wins!");
+    }
+    if(humanChoice=='rock' && computerChoice=='rock')
+    {
+    }
+    if(humanChoice=='paper' && computerChoice=='paper')
+    {
+    }
+    if(humanChoice=='paper' && computerChoice=='scissors')
+    {
+        computerScore +=1;
+        console.log("Computer wins!");
+    }
+    if(humanChoice=='paper' && computerChoice=='rock')
+    {
+        humanScore +=1;
+        console.log("Human wins!");
+    }
+    if(humanChoice=='scissors' && computerChoice=='scissors')
+        {
+        }
+        if(humanChoice=='scissors' && computerChoice=='rock')
+            {
+                    computerScore +=1;
+                    console.log("Computer wins!");
+            }
+            if(humanChoice=='scissors' && computerChoice=='paper')
+                {
+                        humanScore +=1;
+                        console.log("Human wins!");
+                }
+    
+
+}
