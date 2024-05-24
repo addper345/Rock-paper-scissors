e64847e0df712f73388285acdf9c87ae8e67c658
addper345 <25126375+addper345@users.noreply.github.com>
2024-05-23 20:10:27 -0400

    finished playGame() function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -21,6 +21,8 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
     if(humanChoice=='rock' && computerChoice=='paper')
     {
       computerScore +=1;
@@ -33,9 +35,11 @@ function playRound(humanChoice, computerChoice) {
     }
     if(humanChoice=='rock' && computerChoice=='rock')
     {
+        console.log("tie");
     }
     if(humanChoice=='paper' && computerChoice=='paper')
     {
+        console.log("tie");
     }
     if(humanChoice=='paper' && computerChoice=='scissors')
     {
@@ -49,6 +53,7 @@ function playRound(humanChoice, computerChoice) {
     }
     if(humanChoice=='scissors' && computerChoice=='scissors')
         {
+            console.log("tie");
         }
         if(humanChoice=='scissors' && computerChoice=='rock')
             {
@@ -60,6 +65,24 @@ function playRound(humanChoice, computerChoice) {
                         humanScore +=1;
                         console.log("Human wins!");
                 }
+        
     
 
+}
+
+function playGame() {
+    for(let i = 1; i<=5; i++)
+        {
+            playRound(getHumanChoice(), getComputerChoice());
+        }
+    if(humanScore>computerScore) {
+        console.log("You win overall!");
+    }
+    else if (humanScore === computerScore) {
+        console.log("Tie game :|");
+    } else {
+        console.log("You lose :(");
+    }
+    humanScore = 0;
+    computerScore = 0;
 }
